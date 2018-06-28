86f6c1d5072b02efcd59cee2f22ef646f029215d
gbrfix <gabriel.ruffix@gmail.com>
2018-06-28 11:14:42 +0200

    scan optimization

--- main/java/com/gbrfix/randomyzik/DbService.java
+++ main/java/com/gbrfix/randomyzik/DbService.java
@@ -13,6 +13,8 @@ import android.os.IBinder;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 
+import java.util.ArrayList;
+
 /**
  * Created by gab on 27.08.2017.
  */
@@ -67,66 +69,53 @@ public class DbService extends IntentService {
         MediaDAO dao = new MediaDAO(this);
         dao.open();
         SQLiteCursor cursor = dao.getAll();
+        ArrayList<Media> list = new ArrayList<Media>();
+        Cursor c = contentResolver.query(mediaUri,
+                new String[]{MediaStore.Audio.Media._ID, MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.TRACK, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.ARTIST},
+                null, null, null);
+
+        if (c.moveToFirst()) {
+            do {
+                Media media = new Media();
+                media.setMediaId(c.getInt(0));
+                media.setPath(c.getString(1));
+                media.setFlag("unread");
+                media.setTrackNb(c.getString(2));
+                media.setTitle(c.getString(3));
+                media.setAlbum(c.getString(4));
+                media.setArtist(c.getString(5));
+                list.add(media);
+            } while (c.moveToNext());
+        }
 
         if (cursor.getCount() == 0) {
-            Cursor c = contentResolver.query(mediaUri, null, null, null, null);
-            if (c.moveToFirst()) {
-                int idColumn = c.getColumnIndex(MediaStore.Audio.Media._ID);
-                int pathColumn = c.getColumnIndex(MediaStore.Audio.Media.DATA);
-                int trackColumn = c.getColumnIndex(MediaStore.Audio.Media.TRACK);
-                int titleColumn = c.getColumnIndex(MediaStore.Audio.Media.TITLE);
-                int albumColumn = c.getColumnIndex(MediaStore.Audio.Media.ALBUM);
-                int artistColumn = c.getColumnIndex(MediaStore.Audio.Media.ARTIST);
-                do {
-                    Media media = new Media();
-                    media.setMediaId(c.getInt(idColumn));
-                    media.setPath(c.getString(pathColumn));
-                    media.setFlag("unread");
-                    media.setTrackNb(c.getString(trackColumn));
-                    media.setTitle(c.getString(titleColumn));
-                    media.setAlbum(c.getString(albumColumn));
-                    media.setArtist(c.getString(artistColumn));
-                    dao.insert(media);
-                    updated = true;
-                } while (c.moveToNext());
+            for (int i = 0; i < list.size(); i++) {
+                dao.insert(list.get(i));
             }
         } else {
             while (cursor.moveToNext()) {
                 int id = cursor.getInt(0);
                 String path = cursor.getString(1);
                 int media_id = cursor.getInt(2);
-                Cursor c;
-                if (media_id == 0) {
-                    c = contentResolver.query(
-                            mediaUri,
-                            new String[]{MediaStore.Audio.Media._ID, MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.TRACK, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.ARTIST},
-                            "`_data`=?",
-                            new String[]{path},
-                            null
-                    );
+                int i;
+                for (i = 0; i < list.size(); i++) {
+                    boolean found = list.get(i).getMediaId() != 0 ? list.get(i).getMediaId() == media_id : list.get(i).getPath().equals(path);
+                    if (found) {
+                        dao.update(list.get(i), id);
+                        updated = true;
+                        break;
+                    }
+                }
+                if (i >= list.size()) {
+                    dao.remove(id);
+                    updated = true;
                 }
                 else {
-                    c = contentResolver.query(
-                            mediaUri,
-                            new String[]{MediaStore.Audio.Media._ID, MediaStore.Audio.Media.DATA, MediaStore.Audio.Media.TRACK, MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.ALBUM, MediaStore.Audio.Media.ARTIST},
-                            "`_id`=?",
-                            new String[]{String.valueOf(media_id)},
-                            null
-                    );
+                    list.remove(i);
                 }
-                if (c == null) {
-                    dao.remove(id);
-                } else {
-                    Media media = new Media();
-                    c.moveToFirst();
-                    media.setMediaId(c.getInt(0));
-                    media.setPath(c.getString(1));
-                    media.setTrackNb(c.getString(2));
-                    media.setTitle(c.getString(3));
-                    media.setAlbum(c.getString(4));
-                    media.setArtist(c.getString(5));
-                    dao.update(media, id);
-                }
+            }
+            for (int i = 0; i < list.size(); i++) {
+                dao.insert(list.get(i));
                 updated = true;
             }
         }
