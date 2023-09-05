6d1e9931689564eac89cd571123a43b4b4e19973
Gbrfix <gabriel.ruffix@gmail.com>
2023-09-05 06:21:04 +0000

    Show notification on streaming server error

--- main/java/com/gbrfix/randomyzik/MediaPlaybackService.java
+++ main/java/com/gbrfix/randomyzik/MediaPlaybackService.java
@@ -429,6 +429,7 @@ public class MediaPlaybackService extends MediaBrowserServiceCompat implements M
                                     ampSession.connect();
                                 } catch (Exception e) {
                                     Bundle args = new Bundle();
+                                    args.putInt("code", 2);
                                     args.putString("message", e.getMessage());
                                     session.sendSessionEvent("onError", args);
                                     return;
@@ -438,6 +439,7 @@ public class MediaPlaybackService extends MediaBrowserServiceCompat implements M
                                         prepareMediaStreaming(media.getMediaId());
                                     } catch (IOException e) {
                                         Bundle args = new Bundle();
+                                        args.putInt("code", 2);
                                         args.putString("message", e.getMessage());
                                         session.sendSessionEvent("onError", args);
                                         return;
