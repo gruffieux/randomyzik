440256be7b7505ea81a6ff90ed409a46548311a6
Gbrfix <gabriel.ruffix@gmail.com>
2026-07-20 10:38:15 +0200

    Code clean

--- main/java/com/gbrfix/randomyzik/MainActivity.java
+++ main/java/com/gbrfix/randomyzik/MainActivity.java
@@ -722,15 +722,14 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     public void ignoreBatteryOptimization(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            Intent intent = new Intent();
-            String packageName = getPackageName();
-            PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
-            if (!pm.isIgnoringBatteryOptimizations(packageName)) {
-                intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
-                intent.setData(Uri.parse("package:" + packageName));
-                startActivity(intent);
-            }
+        Intent intent = new Intent();
+        String packageName = getPackageName();
+        PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
+
+        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+            intent.setData(Uri.parse("package:" + packageName));
+            startActivity(intent);
         }
     }
 }
