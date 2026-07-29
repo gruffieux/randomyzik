a4871cc1a2c9abdc8ed5ccd38c0aa9793c596604
Gbrfix <gabriel.ruffix@gmail.com>
2026-07-29 09:08:47 +0200

    Code clean

--- main/java/com/gbrfix/randomyzik/MainActivity.java
+++ main/java/com/gbrfix/randomyzik/MainActivity.java
@@ -46,7 +46,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
